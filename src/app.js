a65de9c1d5b362abe040547aa6e3f0a2e73e56b6
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-04-20 07:36:10 +0100

    added cors

--- src/app.js
+++ src/app.js
@@ -9,29 +9,31 @@ const cors = require("cors");
 
 var os = require("os");
 var ifaces = os.networkInterfaces();
+function getIp() {
+  Object.keys(ifaces).forEach(function (ifname) {
+    var alias = 0;
 
-Object.keys(ifaces).forEach(function (ifname) {
-  var alias = 0;
+    ifaces[ifname].forEach(function (iface) {
+      if ("IPv4" !== iface.family || iface.internal !== false) {
+        // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        return;
+      }
 
-  ifaces[ifname].forEach(function (iface) {
-    if ("IPv4" !== iface.family || iface.internal !== false) {
-      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
-      return;
-    }
-
-    if (alias >= 1) {
-      // this single interface has multiple ipv4 addresses
-      console.log(ifname + ":" + alias, iface.address);
-    } else {
-      // this interface has only one ipv4 adress
-      console.log(ifname, iface.address);
-    }
-    ++alias;
+      if (alias >= 1) {
+        // this single interface has multiple ipv4 addresses
+        console.log(ifname + ":" + alias, iface.address);
+      } else {
+        // this interface has only one ipv4 adress
+        console.log(ifname, iface.address);
+      }
+      ++alias;
+    });
   });
-});
+}
 
 var allowedOrigins = ["http://yourapp.com"];
 var getClientIp = function (req) {
+  getIp();
   var ipAddress = req.connection.remoteAddress;
   if (!ipAddress) {
     return "";
