27401d3e9d39eade55ce61f082e83c5ad20aed53
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-04-20 06:53:35 +0100

    finished all

--- src/app.js
+++ src/app.js
@@ -5,38 +5,33 @@ const logger = require("./logger/logger");
 const apiRoute = require("./routes/");
 const path = require("path");
 const cors = require("cors");
-
-const whitelist = ["34.236.76.223"];
-var checkForvalidIp = function (req, callback) {
-  const options = {
-    methods: ["GET", "PUT", "POST", "DELETE", "HEAD", "PATCH"],
-  };
-
-  const currentIpAddress = req.connection.remoteAddress;
-  var ip =
-    (req.headers["x-forwarded-for"] || "").split(",").pop() ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
-  console.log(currentIpAddress, ip);
-  options.origin =
-    whitelist.includes(currentIpAddress) ||
-    whitelist.includes(req.header("Origin"))
-      ? true
-      : false;
-  console.log(options);
-  callback("", options);
+var allowedOrigins = ["http://yourapp.com"];
+var getClientIp = function (req) {
+  var ipAddress = req.connection.remoteAddress;
+  if (!ipAddress) {
+    return "";
+  }
+  // convert from "::ffff:192.0.0.1"  to "192.0.0.1"
+  if (ipAddress.substr(0, 7) == "::ffff:") {
+    ipAddress = ipAddress.substr(7);
+  }
+  return ipAddress;
 };
+const whiteList = [""];
+app.use((req, res, next) => {
+  var ipAddress = getClientIp(req);
+  console.log(ipAddress);
+  if (whiteList.includes(ipAddress)) {
+    next();
+  } else {
+    res.send(ipAddress + " IP is not in whiteList");
+  }
+});
 
-app.use(cors(checkForvalidIp));
 app.use(logger());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-  "/",
-  cors({ origin: "google.com" }),
-  express.static(path.join(__dirname, "/public"))
-);
+app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/api", apiRoute);
 
 module.exports = app;
